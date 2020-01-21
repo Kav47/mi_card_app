2d044850d3fc90297f04b13eeebd5cfeeca2ba04
Kavishan <kavishan47@gmail.com>
2020-01-21 22:59:18 +0000

    Final Version

--- README.md
+++ README.md
@@ -29,3 +29,23 @@ Mi Card is a personal business card. Imagine every time you wanted to give someo
 * So we can utilise hot reload
 * Stateless widget’s build method gets called whenever we create a new version of this widget (MyApp)
 
+## MiCard - How to Build Beautiful UIs with Flutter Widgets
+
+* [MiCard Stub Project](https://github.com/londonappbrewery/mi_card_flutter)
+* [Widgets Catalogue](https://flutter.dev/docs/development/ui/widgets)
+* [Container Documentation](https://docs.flutter.io/flutter/widgets/Container-class.html)
+* [Column Documentation](https://docs.flutter.io/flutter/widgets/Column-class.html)
+* [Row Documentation](https://docs.flutter.io/flutter/widgets/Row-class.html)
+* [Tomek's Flutter Layout Cheat Sheet](https://medium.com/flutter-community/flutter-layout-cheat-sheet-5363348d037e)
+* [Download fancy fonts from fonts.google.com](https://fonts.google.com/)
+* [How to add fonts to the pubspec.yaml file](https://flutter.dev/docs/cookbook/design/package-fonts)
+* [The Icons Class](https://docs.flutter.io/flutter/material/Icons-class.html)
+* [Material Design Icons](https://material.io/tools/icons/?style=baseline)
+* [Icons on materialpalette.com](https://www.materialpalette.com/icons)
+* [Card Class Documentation](https://docs.flutter.io/flutter/material/Card-class.html)
+* [The Padding Class](https://docs.flutter.io/flutter/widgets/Padding-class.html)
+* [The ListTile Class](https://docs.flutter.io/flutter/material/ListTile-class.html)
+* [Flutter's Horizontal Rule - The Divider](https://docs.flutter.io/flutter/material/Divider-class.html)
+* [MiCard Completed Project](https://github.com/londonappbrewery/MiCard-Completed)
+
+[Credits: ](https://github.com/londonappbrewery/MiCard-Completed)
--- lib/main.dart
+++ lib/main.dart
@@ -9,39 +9,85 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.purple,
         body: SafeArea(
-          child: Row(
-//            mainAxisSize: MainAxisSize.min,
-//          verticalDirection: VerticalDirection.up,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//          crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/sombra.jpeg'),
               ),
-               Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.yellow,
+              Text(
+                'Sombra',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico'
+                ),
+              ),
+              Text(
+                'Overwatch Hacker',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 20.0,
+                  color: Colors.purple.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.purple.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.purple,
+                  ),
+                  title: Text(
+                    '+12 345 678 910',
+                    style: TextStyle(
+                        color: Colors.purple.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold
                     ),
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color:  Colors.green,
-                    )
-                  ],
+                  ),
+                ),
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
-              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.purple,
+                  ),
+                  title: Text(
+                    'sombra@*&%^\$\?\$\%\$\.\£\(\)',
+                    style: TextStyle(
+                        color: Colors.purple.shade900,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                ),
+              )
             ],
-          ),
+          )
         ),
       ),
     );
